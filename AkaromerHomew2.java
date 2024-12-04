93f9e42147aeb8373a6ab13258c2e29c7130db17
akaromer525 <akaromer525@gmail.com>
2024-12-04 22:56:11 +0300

    Update AkaromerHomew2.java

--- AkaromerHomew2.java
+++ AkaromerHomew2.java
@@ -1,20 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package akaromer.homew2;
 
 import java.util.Scanner;
 
-/**
- *
- * @author k
- */
 public class AkaromerHomew2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
   Scanner scanner = new Scanner(System.in);
         RealEstateAgency agency = new RealEstateAgency();
